23d1db36ddb9249556ff328aba94ea8e5416332f
lukas <bierling.lukas@gmail.com>
2025-07-07 00:08:03 +0200

    new changes

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -11,16 +11,28 @@ export const Team = (props) => {
             engineering, and applied AI research.
           </p>
         </div>
-        <div id="row">
+        <div className="row">
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
-                  <div className="thumbnail">
-                    {" "}
-                    <img src={d.img} alt="..." className="team-img" />
-                    <div className="caption">
-                      <h4>{d.name}</h4>
-                      <p>{d.job}</p>
+                  <div className="team-card">
+                    <div className="team-card-inner">
+                      {/* Front of the card */}
+                      <div className="team-card-front">
+                        <img src={d.img} alt={d.name} className="team-img" />
+                        <div className="team-info">
+                          <h4>{d.name}</h4>
+                          <p className="team-role">{d.job}</p>
+                        </div>
+                      </div>
+                      {/* Back of the card */}
+                      <div className="team-card-back">
+                        <div className="team-description">
+                          <h4>{d.name}</h4>
+                          <p className="team-role-back">{d.job}</p>
+                          <p className="team-bio">{d.description}</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
