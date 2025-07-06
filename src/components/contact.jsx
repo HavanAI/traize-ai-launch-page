8cd9df64fdb42894ab34c5114e03072eea8ce4cd
lukas <bierling.lukas@gmail.com>
2025-07-07 00:30:37 +0200

    new changes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -39,117 +39,128 @@ export const Contact = (props) => {
     <div>
       <div id="contact">
         <div className="container">
-          <div className="col-md-8">
-            <div className="row">
-              <div className="section-title">
-                <h2>Get In Touch</h2>
-                <p>
-                  Interested in collaborating, partnering, or learning more about our research?
-                  We'd love to hear from potential collaborators and partners.
-                </p>
+          <div className="section-title">
+            <h2>Get In Touch</h2>
+            <p>
+              Interested in collaborating, partnering, or learning more about our research?
+              We'd love to hear from potential collaborators and partners.
+            </p>
+          </div>
+
+          <div className="row">
+            <div className="col-md-12">
+              <div className="contact-form-container">
+                <form name="sentMessage" validate onSubmit={handleSubmit}>
+                  <div className="row">
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <div className="input-wrapper">
+                          <i className="fa fa-user input-icon"></i>
+                          <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            className="form-control modern-input"
+                            placeholder="Your Name"
+                            required
+                            value={name}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-md-6">
+                      <div className="form-group">
+                        <div className="input-wrapper">
+                          <i className="fa fa-envelope input-icon"></i>
+                          <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="form-control modern-input"
+                            placeholder="Your Email"
+                            required
+                            value={email}
+                            onChange={handleChange}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="textarea-wrapper">
+                      <i className="fa fa-comment textarea-icon"></i>
+                      <textarea
+                        name="message"
+                        id="message"
+                        className="form-control modern-textarea"
+                        rows="6"
+                        placeholder="Tell us about your project, collaboration ideas, or questions..."
+                        required
+                        value={message}
+                        onChange={handleChange}
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className="text-center">
+                    <button type="submit" className="btn btn-custom modern-submit">
+                      <i className="fa fa-paper-plane"></i>
+                      Send Message
+                    </button>
+                  </div>
+                </form>
               </div>
-              <form name="sentMessage" validate onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="form-control"
-                        placeholder="Name"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="form-control"
-                        placeholder="Email"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                </div>
-                <div className="form-group">
-                  <textarea
-                    name="message"
-                    id="message"
-                    className="form-control"
-                    rows="4"
-                    placeholder="Message"
-                    required
-                    onChange={handleChange}
-                  ></textarea>
-                  <p className="help-block text-danger"></p>
-                </div>
-                <div id="success"></div>
-                <button type="submit" className="btn btn-custom btn-lg">
-                  Send Message
-                </button>
-              </form>
             </div>
           </div>
-          <div className="col-md-3 col-md-offset-1 contact-info">
-            <div className="contact-item">
-              <h3>Contact Info</h3>
-              <p>
-                <span>
-                  <i className="fa fa-map-marker"></i> Address
-                </span>
-                {props.data ? props.data.address : "loading"}
-              </p>
+
+          <div className="row contact-info-row">
+            <div className="col-md-4">
+              <div className="contact-info-card">
+                <div className="contact-item">
+                  <div className="contact-item-header">
+                    <i className="fa fa-map-marker"></i>
+                    <p className="contact-item-label">Address</p>
+                  </div>
+                  <p className="contact-item-value">
+                    {props.data ? props.data.address : "Munich, Germany"}
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-phone"></i> Phone
-                </span>{" "}
-                {props.data ? props.data.phone : "loading"}
-              </p>
+
+            <div className="col-md-4">
+              <div className="contact-info-card">
+                <div className="contact-item">
+                  <div className="contact-item-header">
+                    <i className="fa fa-phone"></i>
+                    <p className="contact-item-label">Phone</p>
+                  </div>
+                  <p className="contact-item-value">
+                    {props.data ? props.data.phone : "Contact via email"}
+                  </p>
+                </div>
+              </div>
             </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-envelope-o"></i> Email
-                </span>{" "}
-                {props.data ? props.data.email : "loading"}
-              </p>
-            </div>
-          </div>
-          <div className="col-md-12">
-            <div className="row">
-              <div className="social">
-                <ul>
-                  <li>
-                    <a href={props.data ? props.data.facebook : "/"}>
-                      <i className="fa fa-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
-                    </a>
-                  </li>
-                </ul>
+
+            <div className="col-md-4">
+              <div className="contact-info-card">
+                <div className="contact-item">
+                  <div className="contact-item-header">
+                    <i className="fa fa-envelope-o"></i>
+                    <p className="contact-item-label">Email</p>
+                  </div>
+                  <p className="contact-item-value">
+                    {props.data ? props.data.email : "info@traize.com"}
+                  </p>
+                </div>
+
+
               </div>
             </div>
           </div>
         </div>
       </div>
+
       <div id="footer">
         <div className="container text-center">
           <p>
